ee1488bb267dc49fc02cc0ecc4febbf592f58e71
Charmaine Crescini <charmainecrescini@12:1c:29:93:22:21.home>
2025-04-01 17:17:43 -0400

    Playback speed

---
+++ playback-speed.py
@@ -0,0 +1,7 @@
+# Playback speed replaces each space with ... (three dots)
+
+# Ask for user input
+playbackSpeed = input("Type anything ").replace(" ", "...")
+
+# Print new value
+print(playbackSpeed)
